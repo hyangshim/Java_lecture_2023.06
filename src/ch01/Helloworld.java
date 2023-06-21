cfcdc406ae780bfcbfc13779559464d5c3b1ff2d
Choi hyang shim <cuixiang523@gamil.com>
2023-06-21 10:40:23 +0900

    first commit

---
+++ src/ch01/Helloworld.java
@@ -0,0 +1,10 @@
+package ch01;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("안녕하세요?");
+	}
+}
---
+++ src/ch02/Ex01_Variable.java
@@ -0,0 +1,27 @@
+package ch02;
+
+public class Ex01_Variable {
+	
+	public static void main(String[] args) {
+		int age;
+		age = 21;
+		int value = 80;
+		int score = value * 2 ;          //변수를 선언하면서 초기화를 동시에 해주는것을 권장
+		System.out.println("age "+age+", score "+score);
+		
+		int x =10,y =20;
+		// x와 y의 값을 바꾸고 싶을 때
+
+		int tmp =x;
+		x =y;
+		y=tmp;
+		
+		if (score > 100) {
+			int localVar =10;           //if block 안에서만 사용할수 있는 로컬 변수
+			//int value = 20;           // Local 변수 value가 이미 존재하므로 사용할수 없음
+			System.out.println(localVar);
+		}
+		
+	}
+
+}
